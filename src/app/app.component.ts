cd55fdd85d886e84f44490e10fb795232e6cfafc
nkunic <45877400+nkunic@users.noreply.github.com>
2023-01-08 12:20:24 +0100

    5 files modified, 2 added, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  users = [
+    {
+      firstName: 'Frank',
+      lastName: 'Murphy',
+      email: 'frank.murphy@test.com',
+      role: 'User',
+    },
+    {
+      firstName: 'Vic',
+      lastName: 'Reynolds',
+      email: 'vic.reynolds@test.com',
+      role: 'Admin',
+    },
+    {
+      firstName: 'Gina',
+      lastName: 'Jabowski',
+      email: 'gina.jabowski@test.com',
+      role: 'Admin',
+    },
+    {
+      firstName: 'Jessi',
+      lastName: 'Glaser',
+      email: 'jessi.glaser@test.com',
+      role: 'User',
+    },
+    {
+      firstName: 'Jay',
+      lastName: 'Bilzerian',
+      email: 'jay.bilzerian@test.com',
+      role: 'User',
+    },
+  ];
 }
